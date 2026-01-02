371c6cd1fd1cb3bdb70c3fd5b725cc9f7494dd1c
atomu okada gyawali <atomgyawali2@gmail.com>
2026-01-02 18:10:41 +0545

    mycommit

--- lib/core/services/hive/hive_service.dart
+++ lib/core/services/hive/hive_service.dart
@@ -1,48 +1,48 @@
-import 'package:hive/hive.dart';
-import 'package:lost_n_found/core/constants/app_constants.dart';
-import 'package:lost_n_found/features/batch/data/models/batch_hive_model.dart';
-import 'package:path_provider/path_provider.dart';
+// import 'package:hive/hive.dart';
+// import 'package:lost_n_found/core/constants/app_constants.dart';
+// import 'package:lost_n_found/features/batch/data/models/batch_hive_model.dart';
+// import 'package:path_provider/path_provider.dart';
 
-class HiveService {
-  //init
-  Future<void> init() async {
-    final directory = await getApplicationDocumentsDirectory();
-    final path = '${directory.path}/${HiveTableConstant.dbName}';
-  }
+// class HiveService {
+//   //init
+//   Future<void> init() async {
+//     final directory = await getApplicationDocumentsDirectory();
+//     final path = '${directory.path}/${HiveTableConstant.dbName}';
+//   }
 
-  //Register Adapters
-  void _registerAdapter() {
-    if (!Hive.isAdapterRegistered(HiveTableConstant.batchTypeId)) {
-      Hive.registerAdapter(BatchHiveModelAdapter());
-    }
-  }
+//   //Register Adapters
+//   void _registerAdapter() {
+//     if (!Hive.isAdapterRegistered(HiveTableConstant.batchTypeId)) {
+//       Hive.registerAdapter(BatchHiveModelAdapter());
+//     }
+//   }
 
-  //Open Boxes
-  Future<void> openBoxes() async {
-    await Hive.openBox<BatchHiveModel>(HiveTableConstant.batchTable);
-  }
+//   //Open Boxes
+//   Future<void> openBoxes() async {
+//     await Hive.openBox<BatchHiveModel>(HiveTableConstant.batchTable);
+//   }
 
-  //Close boxes
-  Future<void> close() async {
-    await Hive.close();
-  }
+//   //Close boxes
+//   Future<void> close() async {
+//     await Hive.close();
+//   }
 
-  //Queries
+//   //Queries
 
-  Box<BatchHiveModel> get _batchBox =>
-      Hive.box<BatchHiveModel>(HiveTableConstant.batchTable);
+//   Box<BatchHiveModel> get _batchBox =>
+//       Hive.box<BatchHiveModel>(HiveTableConstant.batchTable);
 
-  Future<BatchHiveModel> createBatch(BatchHiveModel model) async {
-    await _batchBox.put(model.batchId, model); //1
-    return model;
-  }
+//   Future<BatchHiveModel> createBatch(BatchHiveModel model) async {
+//     await _batchBox.put(model.batchId, model); //1
+//     return model;
+//   }
 
-  List<BatchHiveModel> getAllBatches(){
-    return _batchBox.values.toList();
-  }
+//   List<BatchHiveModel> getAllBatches(){
+//     return _batchBox.values.toList();
+//   }
 
-  //update
-  Future<void> updateBatch(BatchHiveModel model) async{
-    await _batchBox.values.toList();
-  }
-}
+//   //update
+//   Future<void> updateBatch(BatchHiveModel model) async{
+//     await _batchBox.values.toList();
+//   }
+// }
